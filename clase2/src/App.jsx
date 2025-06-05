f43dfbad143856357220221eee414b69f9ea99c1
Danielsasa <danielsasa1@hotmail.com>
2025-06-04 19:40:12 -0600

    💄 fix button space

--- clase2/src/App.jsx
+++ clase2/src/App.jsx
@@ -5,7 +5,15 @@ function App() {
 
   return (
     <>
-    
+    <h1>Todo List</h1>
+    <input type="text" placeholder='Escribe una tarea' />
+    <button>Añadir</button>
+    <ul>
+      {/* Aquí se mostrarán los elementos de la lista ul>li{Item$}*3>button{Eliminar}*/}
+      <li>Item 1<button>Eliminar</button></li>
+      <li>Item 2<button>Eliminar</button></li>
+      <li>Item 3<button>Eliminar</button></li>
+    </ul>
     </>
   )
 }
