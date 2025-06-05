d1066d5a1eaf15bab5cf88371709f98c56f224ee
Danielsasa <danielsasa1@hotmail.com>
2025-06-04 20:57:32 -0600

    ✨ finisher todo app

--- clase2/src/App.jsx
+++ clase2/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import ToDoItem from './components/ToDoitem'
 
 function App () {
   /* 1. Creo el estado donde guardare la información del input */
   const [inputValue, setInputValue] = useState('')
 
+  /* 4. Creo un estado para guardar las tareas */
+  const [tasks, setTasks] = useState([])
   /* 3. Creo una función que se ejecuta cuando se hace clic a agregar */
   const handleAdd = () => {
-    console.log('Añadir tarea:', inputValue)
+    // console.log('Añadir tarea:', inputValue)
+    setTasks([...tasks, inputValue])
+    setInputValue('') // Limpiar el input después de añadir la tarea
   }
+  /* 5. Creo una función que se ejecuta cuando se hace clic a eliminar */
+  const deleteTodo = (index) => {
+    // console.log('Eliminar tarea en el índice:', index)
+    const newTasks = tasks.filter((_, i) => i !== index)
+    setTasks(newTasks)
+  }
+
   return (
     <>
-      <h1>Todo List</h1>
+      <h1>Lista de Despensa</h1>
       {/* 2. Usar un evento onChange para guardar la información del estado, apenas esta sea tecleada */}
       <input
         type='text'
@@ -22,9 +34,13 @@ function App () {
       <button onClick={handleAdd}>Añadir</button>
       {/* ul>li{Item $}*3>button{Eliminar} */}
       <ul>
-        <li>Item 1<button>Eliminar</button></li>
-        <li>Item 2<button>Eliminar</button></li>
-        <li>Item 3<button>Eliminar</button></li>
+        {tasks.map((task, index) => (
+          <ToDoItem
+            todoName={task}
+            handleDelete={() => deleteTodo(index)}
+            key={index} // Usar el índice como clave, aunque no es lo ideal en producción
+          />
+        ))}
       </ul>
     </>
   )
