932039d5db409fa1acf489434d7ba05038368bc9
Danielsasa <danielsasa1@hotmail.com>
2025-06-04 20:35:21 -0600

    ✨ add todo item compont

--- clase2/src/App.jsx
+++ clase2/src/App.jsx
@@ -1,19 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-  
+function App () {
+  /* 1. Creo el estado donde guardare la información del input */
+  const [inputValue, setInputValue] = useState('')
 
+  /* 3. Creo una función que se ejecuta cuando se hace clic a agregar */
+  const handleAdd = () => {
+    console.log('Añadir tarea:', inputValue)
+  }
   return (
     <>
-    <h1>Todo List</h1>
-    <input type="text" placeholder='Escribe una tarea' />
-    <button>Añadir</button>
-    <ul>
-      {/* Aquí se mostrarán los elementos de la lista ul>li{Item$}*3>button{Eliminar}*/}
-      <li>Item 1<button>Eliminar</button></li>
-      <li>Item 2<button>Eliminar</button></li>
-      <li>Item 3<button>Eliminar</button></li>
-    </ul>
+      <h1>Todo List</h1>
+      {/* 2. Usar un evento onChange para guardar la información del estado, apenas esta sea tecleada */}
+      <input
+        type='text'
+        placeholder='Escribe una tarea'
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={handleAdd}>Añadir</button>
+      {/* ul>li{Item $}*3>button{Eliminar} */}
+      <ul>
+        <li>Item 1<button>Eliminar</button></li>
+        <li>Item 2<button>Eliminar</button></li>
+        <li>Item 3<button>Eliminar</button></li>
+      </ul>
     </>
   )
 }
